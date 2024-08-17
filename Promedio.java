70263f310712341129a80f164f68ded1cc114ce6
RocioSolberg <rosolberg15@gmail.com>
2024-08-17 14:07:01 -0300

    Update Promedio.java

--- Promedio.java
+++ Promedio.java
@@ -1,5 +1,13 @@
 package prograIII;
 
 public class Promedio {
-
+  int suma = 0;
+  int total = 0;
+  for (int i = 0; i<matriz.length; i++){
+    for (int j = 0; i<matriz[i].length;j++){
+      suma+= matriz[i][j];
+      total++;
+    }
+  }
+  double promedio = (double) suma/total;
 }
